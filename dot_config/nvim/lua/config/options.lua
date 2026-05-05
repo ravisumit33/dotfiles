4f89d532d978a9704c7ea7aaabb586b34d44c23e
Sumit Kumar <ravi.sumit33@gmail.com>
2026-05-06 01:48:08 +0530

    Better git diff highlights

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.wrap = true
+vim.opt.diffopt:append("algorithm:histogram,linematch:60") -- intra-line word diff (powers diffview enhanced_diff_hl)
