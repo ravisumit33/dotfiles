90ce86d4849afd5848726aaeddb4c7858c1782f7
Sumit Kumar <ravi.sumit33@gmail.com>
2026-06-18 12:40:58 +0530

    Add keymap to exit terminal mode
    Also comment out agent shortcut as it is not required after
    claudecode.nvim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set({ "n", "t" }, "<c-\\>", function()
-	require("utils.agent").open()
-end, {
-	desc = "Agent",
-})
+-- vim.keymap.set({ "n", "t" }, "<c-\\>", function()
+-- 	require("utils.agent").open()
+-- end, {
+-- 	desc = "Agent",
+-- })
+
+-- Exit terminal mode without Esc, which terminal apps like Claude rely on.
+vim.keymap.set("t", "<C-;>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
